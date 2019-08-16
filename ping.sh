52f7fb74bd54a9e56df43ae7766a7b59ee77542b
kumufengchun <Mayjiao@users.noreply.github.com>
2019-08-16 14:51:26 +0800

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -7,3 +7,18 @@ do
        echo $ip is alive
    fi
 done
+
+
+#或下述写法
+#将循环体放入()&中，()&中的命令会在子shell中执行，而 & 会将其置于后台
+for ip in 192.168.0.{1..255} ;
+do
+    (
+        ping $ip -c2 &> /dev/null ;
+        if [ $? -eq 0 ];
+        then
+        echo $ip is alive
+        fi
+   )&
+done
+wait
